d57a6853628d86b3b3db6bdc5d27bf0177f8a16e
Barragan17 <tegartrahman@gmail.com>
2021-05-25 11:56:03 +0700

    Made Some Changes with Themes and explore testing

--- app/src/main/java/com/example/charaka/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/charaka/ui/explore/ExploreFragment.kt
@@ -6,19 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.charaka.R
+import com.example.charaka.adapter.PostAdapter
+import com.example.charaka.databinding.FragmentExploreBinding
+import com.example.charaka.utils.DataDummy
 
 class ExploreFragment : Fragment() {
 
     private lateinit var exploreViewModel: ExploreViewModel
+    private lateinit var binding: FragmentExploreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        exploreViewModel =
-            ViewModelProvider(this).get(ExploreViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        binding = FragmentExploreBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val postAdapter = PostAdapter()
+        postAdapter.setPost(DataDummy.generatePosts())
+        binding.rvPosts.layoutManager = LinearLayoutManager(context)
+        binding.rvPosts.setHasFixedSize(true)
+        binding.rvPosts.adapter = postAdapter
+        binding.rvPosts.visibility = View.VISIBLE
     }
 }
